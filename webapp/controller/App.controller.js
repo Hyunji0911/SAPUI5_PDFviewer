e984c060674eb4d8ae409e9b65ae159a54b74267
Hyun Ji Lee <hyunji@Hyuns-MacBook-Pro.local>
2023-03-21 11:50:01 +0900

    step 10

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -4,7 +4,14 @@ sap.ui.define(
 		'use strict';
 		return Controller.extend('sap.ui.demo.walkthrough.controller.App', {
 			onShowHello: function () {
-				MessageToast.show('Hello World');
+				// read msg from i18n model
+				var oBundle = this.getView().getModel('i18n').getResourceBundle();
+				var sRecipient = this.getView()
+					.getModel()
+					.getProperty('/recipient/name');
+				var sMsg = oBundle.getText('helloMsg', [sRecipient]);
+				// show message
+				MessageToast.show(sMsg);
 			},
 		});
 	}
